98ff82aee426c0f27e12420e01561c7c3efac515
David Lewis and Kimberly Huynh <kimberly@>
2018-01-17 13:55:50 -0800

    refactor, prototyping, different methods of loop function

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,20 @@
+function vowelFinder(vowels, letters, index, index2) {
+  for (index2 = 0; index2 < vowels.length; index2 +=1){
+		if (letters[index] === vowels[index2]) {
+        letters[index] = ("-");
+  		};
+  	};
+};
+
+//Old vowel finder function
+// function vowelFinder(letters, index) {
+//     if ((letters[index] === "a") || (letters[index] === "e")|| (letters[index] === "i") || (letters[index] === "o") || (letters[index] === "u")){
+//     letters[index] = ("-");
+//   } else {
+//     (letters[index]);
+//   };
+// };
+
 $(document).ready(function () {
   $(".result").hide();
   $("#sentence").submit(function(event){
@@ -5,17 +22,19 @@ $(document).ready(function () {
 
     var string = $("#fullSentence").val();
     var letters = string.split("");
+    var vowels = ["a", "e", "i", "o", "u"];
+    var index2 = 0;
+
+    // var vowel = ("a", "e", "i", "o", "u");
+    // alert(vowel);
+
     // var noVowels = string.replace(/[aeiou]/g, '-');
     // var newArray = letters.map(function(letter)
 
      for (index = 0; index < letters.length; index += 1){
-      if ((letters[index] === "a") || (letters[index] === "e")|| (letters[index] === "i") || (letters[index] === "o") || (letters[index] === "u")){
-        letters[index] = ("-");
-      } else {
-        (letters[index]);
-      };
+       vowelFinder(vowels, letters, index, index2);
     };
-    var noVowels = letters.join(" ");
+    var noVowels = letters.join("");
     $("#puzzle").text(noVowels);
     $("#sentence").hide();
     $(".result").show();
